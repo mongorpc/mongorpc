928301c8bde3b571efbfbc841c729149c2e37bd4
Satish Babariya <satish.babariya@gmail.com>
2021-10-28 20:56:36 +0530

    added unimplemented rpc methods

--- collection.go
+++ collection.go
@@ -1,40 +1,11 @@
 package mongorpc
 
-// func (server *MongoRPCServer) ListCollections(ctx context.Context, in *proto.ListCollectionsRequest) (*proto.ListCollectionsResponse, error) {
-// 	logrus.Printf("ListCollections: %v", in)
-// 	cur, err := server.DB.Database(in.Database).ListCollections(ctx, bson.D{})
-// 	if err != nil {
-// 		return nil, err
-// 	}
+import (
+	"context"
 
-// 	var results bson.A
-// 	for cur.Next(ctx) {
-// 		var result bson.D
-// 		err := cur.Decode(&result)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		results = append(results, result)
-// 	}
-// 	if err := cur.Err(); err != nil {
-// 		return nil, err
-// 	}
-// 	defer cur.Close(ctx)
+	"github.com/mongorpc/mongorpc/proto"
+)
 
-// 	temporaryBytes, err := json.Marshal(results)
-// 	if err != nil {
-// 		logrus.Error(err)
-// 		return nil, err
-// 	}
-// 	var jsonDocuments []interface{}
-// 	err = json.Unmarshal(temporaryBytes, &jsonDocuments)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &proto.ListCollectionsResponse{
-// 		Collections: &proto.ArrayValue{
-// 			Values: ArrayToValue(jsonDocuments),
-// 		},
-// 	}, err
-// }
+func (srv *MongoRPCServer) ListCollections(ctx context.Context, in *proto.ListCollectionsRequest) (*proto.ListCollectionsResponse, error) {
+	return &proto.ListCollectionsResponse{}, nil
+}
